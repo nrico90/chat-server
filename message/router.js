c119c03b658fa2785a1aef53efc37bb35c62eff0
nrico90 <nrico90@gmail.com>
2019-12-13 11:03:07 +0100

    sync and await

--- message/router.js
+++ message/router.js
@@ -6,20 +6,23 @@ const { Router } = require("express");
 
 const router = new Router();
 
-router.get("/message", (req, res, next) => {
-  Message.findAll()
-    .then(messages => {
-      res.send(messages);
-    })
-    .catch(next);
+// router.get("/message", (req, res, next) => {
+//   Message.findAll()
+//     .then(messages => {
+//       res.send(messages);
+//     })
+//     .catch(next);
+// });
+router.get("/message", async (req, res, next) => {
+  const messages = await Message.findAll();
+
+  res.send(messages);
 });
 
-router.post("/message", (req, res, next) => {
-  Message.create(req.body)
-    .then(message => {
-      res.send(message);
-    })
-    .catch(next);
+router.post("/message", async (req, res, next) => {
+  const message = await Message.create(req.body);
+
+  res.send(message);
 });
 
 module.exports = router;
